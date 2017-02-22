88270a6b643287c43b5f88c0d1bc68bb983f4455
s3mac <mack.seb@gmail.com>
2017-02-22 10:59:35 +0100

    added windows batch file

---
+++ i40AS_start_windows.cmd
@@ -0,0 +1,4 @@
+@echo off
+start cmd /k python "C:\Users\Sebastian Mack\Dropbox\Master Thesis\3 Prototyp\i40AS\control_start.py"
+start cmd /k python "C:\Users\Sebastian Mack\Dropbox\Master Thesis\3 Prototyp\i40AS\module_start.py"
+start cmd /k python "C:\Users\Sebastian Mack\Dropbox\Master Thesis\3 Prototyp\i40AS\client_start.py"
