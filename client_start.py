e0e71b6547c0bd4cde72996689fce178ba1ecdf5
s3mac <mack.seb@gmail.com>
2017-03-12 23:53:41 +0100

    changed structure to only modules

--- client_start.py
+++ client_start.py
@@ -1,11 +1,11 @@
 import zmq
 from flask import Flask, jsonify, request, render_template
-from structure import configuration_shell, structure_client
-
+from structure import configuration_shell, structure_module
+import json
 
 
 app = Flask(__name__)
-shell_client = structure_client.entity(configuration_shell.data)
+shell_client = structure_module.entity('client',configuration_shell.data)
 
 
 
@@ -14,23 +14,54 @@ def index():
     return render_template("index.html")
 
 
-@app.route('/api/<string:api_name>', methods=['POST'])
-def api(api_name):
+@app.route('/apis', methods=['GET','POST'])
+def apis():
+    if request.method == 'POST':
+        if request.form:
+            postjson = request.form['JSON']
+            postjson = json.loads(postjson)
+            api_name = postjson['api_name']
+            api_data = postjson['api_data']
+
+            if configuration_shell.data.get(api_name, False):
+
+                MESSAGE = shell_client.create_message(TO = configuration_shell.data[api_name]['identity'], CORE_pyobj = api_data)
+                shell_client.send(MESSAGE)
+                MESSAGE = shell_client.receive()
+
+                return jsonify(shell_client.extract_core(MESSAGE))
+
+            else:
+
+                response = {'response' : 'unknown api'}
+
+                return jsonify(response)
 
 
-    if configuration_shell.data.get(api_name, False):
 
-        MESSAGE = shell_client.create_message(TO = configuration_shell.data[api_name]['identity'], CORE_pyobj = request.get_json())
-        shell_client.send(MESSAGE)
-        MESSAGE = shell_client.receive()
+        if request.json:
+            postjson = request.get_json()
+            api_name = postjson['api_name']
+            api_data = postjson['api_data']
 
-        return jsonify(shell_client.extract_core(MESSAGE))
+            if configuration_shell.data.get(api_name, False):
 
+                MESSAGE = shell_client.create_message(TO = configuration_shell.data[api_name]['identity'], CORE_pyobj = api_data)
+                shell_client.send(MESSAGE)
+                MESSAGE = shell_client.receive()
+
+                return jsonify(shell_client.extract_core(MESSAGE))
+
+            else:
+
+                response = {'response' : 'unknown api'}
+
+                return jsonify(response)
     else:
+        return render_template('api_request.html')
 
-        response = {'response' : 'unknown api'}
 
-        return jsonify(response)
+
 
 
 
--- i40AS_start_windows.cmd
+++ i40AS_start_windows.cmd
@@ -1,4 +1,4 @@
 @echo off
-start cmd /k python "C:\Users\Sebastian Mack\Dropbox\Master Thesis\3 Prototyp\i40AS\control_start.py"
-start cmd /k python "C:\Users\Sebastian Mack\Dropbox\Master Thesis\3 Prototyp\i40AS\module_start.py"
-start cmd /k python "C:\Users\Sebastian Mack\Dropbox\Master Thesis\3 Prototyp\i40AS\client_start.py"
+start cmd /k python "C:\Users\Sebastian Mack\Dropbox\Studium\2 Maschinenbau Master of Science\Master Thesis\3 Prototyp\i40AS\control_start.py"
+start cmd /k python "C:\Users\Sebastian Mack\Dropbox\Studium\2 Maschinenbau Master of Science\Master Thesis\3 Prototyp\i40AS\module_start.py"
+start cmd /k python "C:\Users\Sebastian Mack\Dropbox\Studium\2 Maschinenbau Master of Science\Master Thesis\3 Prototyp\i40AS\client_start.py"
