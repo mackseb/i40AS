2e1d6c2b66d3458b4ee6ec3e631f03b859a040d9
s3mac <mack.seb@gmail.com>
2017-03-13 11:52:34 +0100

    services with $User

--- i40AS_install_unix.sh
+++ i40AS_install_unix.sh
@@ -10,3 +10,18 @@ virtualenv projectenv
 source projectenv/bin/activate
 
 pip install -r requirements.txt
+
+cp control.service /etc/systemd/system/
+
+cp backend.service /etc/systemd/system/
+
+cp frontend.service /etc/systemd/system/
+
+systemctl start control
+systemctl enable control
+
+systemctl start backend
+systemctl enable backend
+
+systemctl start frontend
+systemctl enable frontend
