147b6bff12cd2d8952841e6083012a5be732a474
s3mac <mack.seb@gmail.com>
2017-03-13 21:46:08 +0100

    update6

--- i40AS_install_unix.sh
+++ i40AS_install_unix.sh
@@ -9,32 +9,31 @@ virtualenv projectenv
 
 source projectenv/bin/activate
 
-pip install -r requirements.txt
+pip install -r /install/requirements.txt
 
 deactivate
 
-NOWUSER= who am i | awk '{print $1}'
-sed -i -e "s/USR/$NOWUSER/g" control.service
-sed -i -e "s/USR/$NOWUSER/g" frontend.service
-sed -i -e "s/USR/$NOWUSER/g" backend.service
-sed -i -e "s/USR/$NOWUSER/g" nginx
+
+sed -i -e "s/USR/$(whoami)/g" control.service
+sed -i -e "s/USR/$(whoami)/g" frontend.service
+sed -i -e "s/USR/$(whoami)/g" backend.service
+sed -i -e "s/USR/$(whoami)/g" nginx
 ADDRESS=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
 sed -i -e "s/ADD/$ADDRESS/g" nginx
 
-cp control.service /etc/systemd/system
+cp /install/control.service /etc/systemd/system
 
-cp backend.service /etc/systemd/system
+cp /install/backend.service /etc/systemd/system
 
-cp frontend.service /etc/systemd/system
+cp /install/frontend.service /etc/systemd/system
 
-cp nginx /etc/nginx/sites-available
+cp /install/nginx /etc/nginx/sites-available
 ln -s /etc/nginx/sites-available/nginx /etc/nginx/sites-enabled
 
 systemctl start control
 systemctl enable control
 
 systemctl start backend
-systemctl enable backend
 
 systemctl start frontend
 systemctl enable frontend
