48348ae9942c66fc4954715faadffc53fad2cd85
s3mac <mack.seb@gmail.com>
2017-03-18 21:47:29 +0100

    second change readable

--- structure/config.py
+++ structure/config.py
@@ -1,14 +1,14 @@
 data_windows =
 {
 'HTTPIN': {'identity': b'httpin','url':'tcp://127.0.0.1:5555'},
-'HTTPOUT': {'identity': b'httpout','url': 'tcp://127.0.0.1:5558'},
-'DATABASE': {'identity': b'database','url': 'tcp://127.0.0.1:5556'},
-'SYSTEM': {'identity': b'system','url': 'tcp://127.0.0.1:6000'},
-'INTERFACE': {'identity': b'interface','url': 'tcp://127.0.0.1:5557'},
-'API_MAINTENANCE': {'identity': b'api_maintenance','url': 'tcp://127.0.0.1:5559'},
+'HTTPOUT': {'identity': b'httpout','url': 'tcp://127.0.0.1:5556'},
+'DATABASE': {'identity': b'database','url': 'tcp://127.0.0.1:5557'},
+'SYSTEM': {'identity': b'system','url': 'tcp://127.0.0.1:5558'},
+'INTERFACE': {'identity': b'interface','url': 'tcp://127.0.0.1:5559'},
+'API_MAINTENANCE': {'identity': b'api_maintenance','url': 'tcp://127.0.0.1:6000'},
 }
 
-data_unix =
+data_linux =
 {
 'HTTPIN': {'identity': b'httpin','url':'ipc://httpin.ipc'},
 'HTTPOUT': {'identity': b'httpout','url': 'ipc://httpout.ipc'},
