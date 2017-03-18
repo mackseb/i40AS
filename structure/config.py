eb9b5dc8e6ab98559675c13c1b09101f266f0a32
s3mac <mack.seb@gmail.com>
2017-03-18 21:30:29 +0100

    config file readable

--- structure/config.py
+++ structure/config.py
@@ -1,22 +1,21 @@
-
-data_windows = {
+data_windows =
+{
 'HTTPIN': {'identity': b'httpin','url':'tcp://127.0.0.1:5555'},
 'HTTPOUT': {'identity': b'httpout','url': 'tcp://127.0.0.1:5558'},
 'DATABASE': {'identity': b'database','url': 'tcp://127.0.0.1:5556'},
 'SYSTEM': {'identity': b'system','url': 'tcp://127.0.0.1:6000'},
 'INTERFACE': {'identity': b'interface','url': 'tcp://127.0.0.1:5557'},
 'API_MAINTENANCE': {'identity': b'api_maintenance','url': 'tcp://127.0.0.1:5559'},
-
 }
 
-data_unix = {
+data_unix =
+{
 'HTTPIN': {'identity': b'httpin','url':'ipc://httpin.ipc'},
 'HTTPOUT': {'identity': b'httpout','url': 'ipc://httpout.ipc'},
 'DATABASE': {'identity': b'database','url': 'ipc://database.ipc'},
 'SYSTEM': {'identity': b'system','url': 'ipc://system.ipc'},
 'INTERFACE': {'identity': b'interface','url': 'ipc://interface.ipc'},
 'API_MAINTENANCE': {'identity': b'api_maintenance','url': 'ipc://api_maintenance.ipc'},
-
 }
 
 
