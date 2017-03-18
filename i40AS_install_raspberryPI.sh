0f320ecc07bf88abcfcd2867246acbaa04416a0f
s3mac <mack.seb@gmail.com>
2017-03-18 11:53:15 +0100

    new raspberry installation and removed logs

---
+++ i40AS_install_raspberryPI.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+#install python3.5 if necessary
+#cd ~
+#wget https://www.python.org/ftp/python/3.5.1/Python-3.5.1.tgz
+#tar -zxvf Python-3.5.1.tgz
+#cd Python-3.5.1
+#./configure && make && sudo make install
+
+#pip install --upgrade virtualenv
+
+#rasp stretch update
+#sudo apt-get update
+#sudo apt-get dist-upgrade
+#sudo apt-get autoremove
+
+
+
+apt-get update  # To get the latest package lists
+apt-get install python3-pip python3-dev nginx -y
+
+
+pip3 install virtualenv
+
+virtualenv -p python3 projectenv
+
+source projectenv/bin/activate
+
+pip install -r install/requirements.txt
+
+deactivate
+
+sed -i -e "s@DIR@$PWD@g" install/control.service
+sed -i -e "s@USR@$SUDO_USER@g" install/control.service
+
+sed -i -e "s@DIR@$PWD@g" install/wsgi.service
+sed -i -e "s@USR@$SUDO_USER@g" install/wsgi.service
+
+sed -i -e "s@DIR@$PWD@g" install/frontend.service
+sed -i -e "s@USR@$SUDO_USER@g" install/frontend.service
+
+sed -i -e "s@DIR@$PWD@g" install/backend.service
+sed -i -e "s@USR@$SUDO_USER@g" install/backend.service
+
+sed -i -e "s@DIR@$PWD@g" install/nginx
+ADDRESS=`ifconfig wlan0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
+
+#eth0
+
+
+sed -i -e "s@ADD@$ADDRESS@g" install/nginx
+
+cp install/control.service /etc/systemd/system
+
+cp install/wsgi.service /etc/systemd/system
+
+cp install/frontend.service /etc/systemd/system
+
+cp install/backend.service /etc/systemd/system
+
+cp install/nginx /etc/nginx/sites-available
+ln -s /etc/nginx/sites-available/nginx /etc/nginx/sites-enabled
+
+systemctl start control
+systemctl enable control
+
+systemctl start wsgi
+systemctl enable wsgi
+
+systemctl start frontend
+systemctl enable frontend
+
+systemctl start backend
+systemctl enable backend
+
+systemctl daemon-reload
+
+systemctl restart nginx
